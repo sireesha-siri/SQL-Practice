db1b56d76e59027e66dd213953ce129f27547767
Sireesha <agurusireeshaedu@gmail.com>
2023-10-18 11:08:04 +0530

    Create Create_Table.sql

---
+++ Create_Table.sql
@@ -0,0 +1,23 @@
+create database mylearing;
+
+show databases;
+
+drop database mylearing;
+
+create database mylearning;
+
+use mylearning;
+
+show tables;
+
+create table player(
+name varchar(200),
+age int,
+score integer);
+
+describe player;
+
+
+
+
+
