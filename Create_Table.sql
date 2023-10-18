8f8c497601b9d77ac9ef25a6fc19862a57b03de4
Sireesha <agurusireeshaedu@gmail.com>
2023-10-18 11:34:02 +0530

    Create Create_Table.sql

---
+++ Create_Table.sql
@@ -0,0 +1,23 @@
+create database mylearing;
+
+show databases;
+
+drop database mylearing;
+
+create database mylearning; -- creating a databse
+
+use mylearning; -- using the databse
+
+show tables; -- displaying the tables in the databse
+
+create table player(   -- creating a table inside the databse
+name varchar(200),
+age int,
+score integer);
+
+describe player;  -- displaying the table
+
+
+
+
+
