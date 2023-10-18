9bd3b10e6261d1f1ea8ef8b8c016ac379a55c7b1
Sireesha <agurusireeshaedu@gmail.com>
2023-10-18 11:50:37 +0530

    Create Retrieving_Data.sql

---
+++ Introduction to SQL/Retrieving_Data.sql
@@ -0,0 +1,9 @@
+use mylearning; -- using database
+
+show tables; -- displaying the tables
+
+select * from player; -- retrieving complete data
+
+select name, age from player; -- retrieving specific data
+
+select * from player where name = 'Sai'; -- retrieving the data on specific condition
